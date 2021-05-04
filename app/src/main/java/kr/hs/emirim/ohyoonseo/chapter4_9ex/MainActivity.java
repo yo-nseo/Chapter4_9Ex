55b0235ee080fcc3c1c18421575f5ac26c261c0d
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-05-04 19:51:09 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/chapter4_9ex/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.ohyoonseo.chapter4_9ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
